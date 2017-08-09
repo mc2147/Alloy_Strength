f9bf5e0132752a89f50ef24786e6c8fbe72b3190
Amy Sitwala <asitwala@Amys-MacBook-Pro.local>
2017-08-08 20:29:50 -0500

    Removing .git files and adding static and templates directories

--- AlloyStrength/urls.py
+++ AlloyStrength/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url
 from django.contrib import admin
-from Users.views import Home 
+from Users.views import Home, Member_Home, Admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^admin-site/', Admin, name='Home'),
     url(r'^home/', Home, name='Home'),
+    url(r'^member-home/', Member_Home, name='Home'),
 
 ]
